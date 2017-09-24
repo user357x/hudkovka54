8da5bbd20ce2566b2275c4443b3a23af1a855521
sergey <user357x@yandex.ru>
2017-09-24 17:59:26 +0700

    check

--- router/exclusivnaya-kovka.js
+++ router/exclusivnaya-kovka.js
@@ -6,6 +6,30 @@ module.exports = (req, res, next) => co(function* () {
 
     //const gallery = yield co(getGallery(filename));
 
+    const gallery_src = `/img/gallery/${filename}`;
+
+    const gallery_path = `${result_path}${gallery_src}`;
+
+    const small_gallery_src = `${gallery_src}/small`;
+
+    const small_gallery_path = `${result_path}${small_gallery_src}`;
+
+    const items = yield fs.readDir(gallery_path);
+
+    const images = items.filter(img => img.split('.').length === 2);
+
+    yield Promise.all(
+        images.map(img => {
+            return new Promise((resolve, reject) => {
+                im.resize({
+                    srcPath: `${gallery_path}/${img}`,
+                    dstPath: `${small_gallery_path}/${img}`,
+                    width: 150
+                }, err => err ? reject(err) : resolve());
+            })
+        })
+    );
+
     res.status(200).send(filename);
 
     /*yield save(

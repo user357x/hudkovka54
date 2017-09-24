0767dc0b25905ed0a183e3d2cd6c90e918966237
sergey <user357x@yandex.ru>
2017-09-24 15:42:09 +0700

    check

--- config.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-module.exports = {
-    port : 3000,
-    static_port : 3001,
-    environment : 'development',
-    result_path : `${__dirname}/result`,
-    smallImgWidth : 150
-};

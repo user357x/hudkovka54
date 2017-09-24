05778daf8ea212ce2a723fb3c86bdd74992f5049
sergey <user357x@yandex.ru>
2017-09-24 18:04:39 +0700

    check

--- router/exclusivnaya-kovka.js
+++ router/exclusivnaya-kovka.js
@@ -22,14 +22,18 @@ module.exports = (req, res, next) => co(function* () {
 
     const images = items.filter(img => img.split('.').length === 2);
 
-    yield Promise.all(
+    return yield Promise.all(
         images.map(img => {
             return new Promise((resolve, reject) => {
-                im.resize({
-                    srcPath: `${gallery_path}/${img}`,
-                    dstPath: `${small_gallery_path}/${img}`,
-                    width: 150
-                }, err => err ? reject(err) : resolve());
+                im.identify(`${gallery_path}/${img}`, (err, features) => {
+                    err ? reject(err) :
+                        resolve({
+                            width : features.width,
+                            height : features.height,
+                            src : `${gallery_src}/${img}`,
+                            smallSrc : `${small_gallery_src}/${img}`
+                        })
+                });
             })
         })
     );
